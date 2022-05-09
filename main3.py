d753b8619ef82141503a39531a7a12781889a3c4
Exe322 <75399131+Exe322@users.noreply.github.com>
2022-05-09 18:34:46 -0400

    Update main3.py

--- main3.py
+++ main3.py
@@ -1,23 +1,17 @@
 #!/usr/bin/env python3
-import RPi.GPIO as GPIO  # importa GPIO
-from hx711 import HX711  # importa la clase HX711
-import csv
-from datetime import datetime
-import time
-import itertools
-import tkinter as tk
+import RPi.GPIO as GPIO       # importa GPIO
+from hx711 import HX711       # importa HX711
+import csv                    # importa CVS
+from datetime import datetime # importa  datetime
+import time                   # importa time
+import tkinter as tk          # importa tkinter
 from tkinter import ttk
-from tkinter import *
-from tkinter.messagebox import showinfo
-from threading import Thread
-
-GPIO.setwarnings(False)  # elimina los warnings
+GPIO.setwarnings(False)       # Omite warnings para impedir la interrupción de la rutina
 
 def balanza_comida():
     try:
         GPIO.setmode(GPIO.BCM)  # Pines GPIO en numeración BCM
-        # Crea un objeto hx que represente el chip HX711 real
-        # Los parámetros de entrada obligatorios son solo 'Pin_Dato' y 'PD_sck'
+        # Crea un objeto hx que  Los parámetros de entrada obligatorios son solo 'Pin_Dato' y 'PD_sck'
         global hx
         hx = HX711(dout_pin=5, pd_sck_pin=6)
         # Medir la tara y guardar el valor como compensación para el canal actual
@@ -25,7 +19,7 @@ def balanza_comida():
         err = hx.zero()
         # Verifica si todo está correcto
         if err:
-            raise ValueError('La tara no se puede definir.')
+            raise ValueError('La tara no se puede definir')
 
         reading = hx.get_raw_data_mean()
         if reading:  # Verificar si el valor correcto
@@ -56,19 +50,14 @@ def balanza_comida():
             print('Relación de peso establecida.')
         else:
             raise ValueError('No se puede calcular el valor medio . ERROR', reading)
-
-        # Leer datos varias veces y devolver el valor medio
-        # restado por compensación y escalado a las
-        # unidades deseadas. En este caso en gramos.
-        print("Ahora, leeré datos en un bucle infinito. Para salir presione 'CTRL + C'")
-        #input('Presione Enter para comenzar a leer')
-        #print('El peso actual en la balanza en gramos es: ')
-        print('Ahora comenzara lectura de datos.')
+        
+        #mostrar el comienzo de la funcion balanza_agua
+        print('Ahora comenzara la compensacion y tara del deposito de agua')
         balanza_agua()
 
       
     except (KeyboardInterrupt, SystemExit):
-        print('Chau :)')
+        print('Finalizacion de la lectura')
 
     finally:
         GPIO.cleanup()
@@ -116,77 +105,97 @@ def balanza_agua():
         else:
             raise ValueError('No se puede calcular el valor medio . ERROR', readingy)
 
-        # Leer datos varias veces y devolver el valor medio
-        # restado por compensación y escalado a las
-        # unidades deseadas. En este caso en gramos.
+        
+        # mostrar comienzo del bucle de lectura y medicion del volumen de agua en el deposito
         print("Ahora, leeré datos en un bucle infinito. Para salir presione 'CTRL + C'")
-        print('Lectura de agua')
-        # print('El peso actual en la balanza en gramos es: ')
+        print('Lectura del volumen de agua en el deposito')
+        #llamado de la funcion data_cvs
         data_cvs()
     except (KeyboardInterrupt, SystemExit):
-        print('Chau :)')
+        print('Finalizacion de la lectura')
 
     finally:
         GPIO.cleanup()
         
 def data_cvs():
+    #crea un archivo llamado datos.cvs si no existe y lo abre. 
     with open('datos.csv', mode='w') as csv_file:
+        # crea columans con los nombres id_dispositivo, correlativo de lectura, peso leido, fecha, hora
         fieldnames = ['id_deposito', 'correlativo_lectura', 'peso_leido', 'fecha', 'hora']
+        # varible para escibir en la basde de datos
         writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
         writer.writeheader()
+        #valor inicial del contador para el correlativo de lectura
         i=0
-        
+        # Leer datos durante la ejecucion del ciclo, devolver el valor medio de peso y agua. ademas de escribir los datos en el cvs y mostrarlos en la ventana root
         while True:
+            # contador incemental en 1
             i += 1            
+            # variables de lectura de peso y agua por parte de HX711
             peso = round(hx.get_weight_mean(30),2)
             agua = round(hy.get_weight_mean(30),2)
+            #variables de porcentaje para la barra de progreso y label
             Comida.step(round((peso*100)/pesomax))
             Agua.step(round(agua*100/pesomax))
+            # varibles visuales para la interfas grafica de la lecturas del sensor de carga
             value_label = ttk.Label(root, text=f"     Alimento disponible  {Comida['value']}%  {peso} gr     ")
             value_label.grid(column=1, row=0, columnspan=3, padx=20, pady=10)
             value_label2 = ttk.Label(root, text=f"     Agua disponible: {Agua['value']}%  {agua} ml    ")
             value_label2.grid(column=1, row=2, columnspan=2, padx=20, pady=10)
+            #muestra en el pront el valor de las variables de los sensores de carga
             print("El peso actual en gramos es de %.2f" % peso)
             print("Volumen de agua en ML es de %.2f" % agua)
+            #escribe en el arcivo dato.cvs los datos recopiladores de las varibles del deposito de agua, correlativo de lectura, peso leido, fecha y hora
             writer.writerow({'id_deposito': 'Agua', 'correlativo_lectura': str(i), 'peso_leido': str(agua),
                              'fecha': str(datetime.now().strftime('%Y-%m-%d')),
                              'hora': str(datetime.now().strftime('%H:%M:%S'))})
+            #escribe en el arcivo dato.cvs los datos recopiladores de las varibles del deposito de comida, correlativo de lectura, peso leido, fecha y hora
             writer.writerow({'id_deposito': 'Comida', 'correlativo_lectura': str(i), 'peso_leido': str(peso),
                              'fecha': str(datetime.now().strftime('%Y-%m-%d')),
                              'hora': str(datetime.now().strftime('%H:%M:%S'))})
-            
+            #actualiza los parametros en la ventana root
             root.update()
+            #resta el valores de la barra de progreso para evitar que se sumen los valores por la funcion while True
             Comida.step(round(-(peso*100)/pesomax))
             Agua.step(round(-agua*100/pesomax))
-        
+        # termino de eventos de tkinter
         root.mainloop()
-        
+        #tiempo de reposo o espera de 5 segundos
         time.sleep(5)    
             
                             
 def dashboard():
+    #variable global de root
     global root
+    #asignar varible de tkinter
     root = tk.Tk()
+    #Tamaño de la venta
     root.geometry('330x180')
+    #titulo de la ventana
     root.title('Visualizacion de datos')
+    #variable global del peso amximo de los deposito decomida y agua
     global pesomax
+    #asignar valor a la variable de pesomaximo en unidades de gramos
     pesomax=500
-
+    #varible global de Comida
     global Comida
+    #barra de progreso del deposito de comida y caracteristicas de posicion, modo y largo
     Comida = ttk.Progressbar(
     root,
     orient='horizontal',
     mode='determinate',
     length=180
     )
+    #varible global de Agua
     global Agua
+    #barra de progreso del deposito de agua y caracteristicas de posicion, modo y largo
     Agua = ttk.Progressbar(
     root,
     orient='horizontal',
     mode='determinate',
     length=180)
 
-    # place the progressbar
+    # posicion de las barras de progreso de comida y agua
     Comida.grid(column=1, row=1, columnspan=2, padx=45, pady=5, sticky='e')
     Agua.grid(column=1, row=3, columnspan=2, padx=45, pady=5, sticky='e')
     
@@ -194,8 +203,11 @@ def dashboard():
     
 
 if __name__ == '__main__':
+    #ejecucion de las funciones de dashboar y balanza_comida
     dashboard()
     balanza_comida()
+    
+    
     
     
     
